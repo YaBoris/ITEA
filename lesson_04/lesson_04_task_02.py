58e8f3fb7533a0a427efdd7fd1d8726630b5cf8c
YaBoris <7772962+YaBoris@users.noreply.github.com>
2020-07-21 23:13:53 +0300

    Соц.сеть

---
+++ lesson_04/lesson_04_task_02.py
@@ -0,0 +1,21 @@
+class Registration:
+    def __init__(self):
+        pass
+
+    def enter_data(self):
+        pass
+
+
+class Authorization(Registration):
+    def __init__(self):
+        super(Authorization, self).__init__()
+        pass
+
+
+class User:
+    def __init__(self):
+        pass
+
+    def separate_rules(self):
+        pass
+
