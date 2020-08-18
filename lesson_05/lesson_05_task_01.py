e1eab2cc1a49256618fe0ff89db99426a62f1993
YaBoris <7772962+YaBoris@users.noreply.github.com>
2020-08-18 20:44:26 +0300

    Переделал код, функция обернутая в декоратор, пустая.

--- lesson_05/lesson_05_task_01.py
+++ lesson_05/lesson_05_task_01.py
@@ -1,45 +1,48 @@
 from threading import Thread
-from time import sleep
 
 thread_data = []
+MAX_VALUE = 500
 
 
 def check_is_daemon(thr_data):
     def decorator_thread(func):
         def wrapper():
-            t = Thread(target=func, name=thread_data[0], daemon=thr_data[1])
-            print(f"Thread \"{thread_data[0]}\" before sleep")
-            t.start()
-            print(f"Thread \"{thread_data[0]}\" after sleep")
+            thr_data[0][0].daemon = thr_data[0][1]
+            if thr_data[0][1]:
+                print(f"Thread \"{thr_data[0][0].name}\" started as daemon. State {thr_data[0][1]}\n")
+                thr_data[0][0].start()
+            else:
+                print(f"Thread \"{thr_data[0][0].name}\" started as regular. State {thr_data[0][1]}\n")
+                thr_data[0][0].start()
         return wrapper
     return decorator_thread
 
 
 def work_with_threads():
-    print("Call new thread: ")
-    thr_name = input()
-    print(f"This thread \"{thr_name}\" is daemon? Yes/No")
+    print(f"This thread is daemon? Yes/No")
     while True:
         daemon_status = input()
         if daemon_status == "Yes":
-            daemon = True
-            break
+            return True
         elif daemon_status == "No":
-            daemon = False
-            break
+            return False
         else:
-            print("Enter correct option - \'Yes\' or \'No\'")
-    return thr_name, daemon
+            print("Enter correct option - \'Yes\' or \'No\'\n")
 
 
 @check_is_daemon(thread_data)
+def nothing_to_do():
+    pass
+
+
 def some_function():
-    sleep(3)
-    print("Function is finished")
+    print("Function started\n")
+    for index in range(MAX_VALUE):
+        print(f"index {index} = {index ** MAX_VALUE}")
+    print("Function completed\n")
 
 
 if __name__ == "__main__":
-    thread_info = work_with_threads()
-    thread_data.append(thread_info[0])
-    thread_data.append(thread_info[1])
-    some_function()
+    is_daemon = work_with_threads()
+    thread_data.append((Thread(target=some_function), is_daemon))
+    nothing_to_do()
