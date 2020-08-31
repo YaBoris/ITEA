aa5dca6923cc225d36d5170f10853918b6d3341e
YaBoris <7772962+YaBoris@users.noreply.github.com>
2020-08-31 17:22:21 +0300

    Создал класс MyList. Сделал его итерируемым. Переопределил оператор сложения для создания расширенного списка

--- lesson_06/lesson_06_task_01/my_list.py
+++ lesson_06/lesson_06_task_01/my_list.py
@@ -5,12 +5,6 @@ class MyList(list):
         super(MyList, self).__init__()
         self._data = []
 
-    def _key_is_valid(self, index):
-        if 0 < index < len(self._data):
-            return True
-        else:
-            raise IndexError
-
     def __getitem__(self, item):
         if self._key_is_valid(item):
             return self._data[item]
@@ -23,16 +17,33 @@ class MyList(list):
         if self._key_is_valid(key):
             del self._data[key]
 
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.cursor+1 <= len(self._data):
+            temp_value = self._data[self.cursor]
+            self.cursor += 1
+            return temp_value
+        else:
+            raise StopIteration
+
+    def _key_is_valid(self, index):
+        if 0 < index < len(self._data):
+            return True
+        else:
+            raise IndexError
+
     def pop(self, key=-1):
         if self._key_is_valid(key):
             del self._data[key-1]
 
     def append(self, value):
-        self._data.extend(self.get_list_for_append(self, value))
+        self._data.extend(self.get_list_for_append(value))
 
     def insert(self, key, value):
         if self._key_is_valid(key):
-            self._data.extend(self.get_list_for_append(self, 0))
+            self._data.extend(self.get_list_for_append(0))
             for index in range(len(self._data)):
                 self._data[len(self._data) - 1 - index] = self._data[len(self._data) - 1 - index - 1]
                 if len(self._data) - 1 - index == key:
@@ -51,15 +62,16 @@ class MyList(list):
             raise ValueError
 
     def clear(self):
-        if len(self._data) != 0:
-            for index in range(len(self._data)-1, 0):
-                del self._data[index]
+        size = len(self._data)
+        if size > 0:
+            for index in range(size):
+                del self._data[size - 1 - index]
 
     def __add__(self, other):
-        new_list = MyList()
-        new_list.extend(self._data)
-        new_list.extend(other.get_list())
-        return new_list
+        temp_list = [0]
+        temp_list.extend(self._data)
+        temp_list.extend(other.get_list())
+        return MyList(temp_list)
 
     def print_(self):
         print(self._data)
@@ -68,6 +80,5 @@ class MyList(list):
         return self._data
 
     @staticmethod
-    def get_list_for_append(self, value):
-        temp_list = [value]
-        return temp_list
+    def get_list_for_append(value):
+        return [value]
