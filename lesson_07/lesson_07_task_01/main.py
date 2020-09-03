56d3998c10f60ef598721f72d59eae0f7a8636a9
YaBoris <7772962+YaBoris@users.noreply.github.com>
2020-09-03 14:58:54 +0300

    Дополнил SELECT запросом

--- lesson_07/lesson_07_task_01/main.py
+++ lesson_07/lesson_07_task_01/main.py
@@ -7,17 +7,15 @@ if __name__ == '__main__':
 
         sql_req = "INSERT INTO test_table(INFORMATION) VALUES ('set_data1');"
         cursor.execute(sql_req)
-        conn.commit()
-        print(cursor.fetchall())
         sql_req = "INSERT INTO test_table(INFORMATION) VALUES ('set_data2');"
         cursor.execute(sql_req)
-        conn.commit()
-        print(cursor.fetchall())
         sql_req = "INSERT INTO test_table(INFORMATION) VALUES ('set_data3');"
         cursor.execute(sql_req)
-        conn.commit()
-        print(cursor.fetchall())
         sql_req = "INSERT INTO test_table(INFORMATION) VALUES ('set_data4');"
         cursor.execute(sql_req)
         conn.commit()
+        print(cursor.fetchall())
+
+        sql_req = "SELECT * FROM test_table;"
+        cursor.execute(sql_req)
         print(cursor.fetchall())
